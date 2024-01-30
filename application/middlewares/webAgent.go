ff916cf9335445267b4785df01ce3cb9d76443bd
Emekarr <aceanyanwu02@gmail.com>
2024-01-30 18:46:43 +0100

    discard webtoken for web endpoint

--- application/middlewares/webAgent.go
+++ application/middlewares/webAgent.go
@@ -1,24 +1,20 @@
 package middlewares
 
 import (
-	"os"
-
-	apperrors "kego.com/application/appErrors"
 	"kego.com/application/interfaces"
-	"kego.com/infrastructure/cryptography"
 )
 
 
 func WebAgentMiddleware(ctx *interfaces.ApplicationContext[any], ipAddress string) (*interfaces.ApplicationContext[any], bool) {
-	clientKey := ctx.GetHeader("Web-Api-Key")
-	if clientKey == nil {
-		apperrors.AuthenticationError(ctx.Ctx, "missing web token")
-		return nil, false
-	}
-	valid := cryptography.CryptoHahser.VerifyData(os.Getenv("WEB_CLIENT_API_KEY"), clientKey.(string))
-	if !valid {
-		apperrors.AuthenticationError(ctx.Ctx, "invalid web token")
-		return nil, false
-	}
+	// clientKey := ctx.GetHeader("Web-Api-Key")
+	// if clientKey == nil {
+	// 	apperrors.AuthenticationError(ctx.Ctx, "missing web token")
+	// 	return nil, false
+	// }
+	// valid := cryptography.CryptoHahser.VerifyData(os.Getenv("WEB_CLIENT_API_KEY"), clientKey.(string))
+	// if !valid {
+	// 	apperrors.AuthenticationError(ctx.Ctx, "invalid web token")
+	// 	return nil, false
+	// }
 	return ctx, true
 }
